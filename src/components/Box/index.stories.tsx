142cce6535b7e5da3110e6a677f7055329435ca6
zsj <zousjun@126.com>
2021-11-18 14:54:21 +0800

    更新Box的stories说明

--- src/components/Box/index.stories.tsx
+++ src/components/Box/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Box from "./index";
 
@@ -7,8 +7,38 @@ export default {
   component: Box,
 } as ComponentMeta<typeof Box>;
 const Template: ComponentStory<typeof Box> = (args) => (
-  <Box isLoading={false} data={[112]}>
-    确定
-  </Box>
+  <Box {...args}>{args.children}</Box>
 );
-export const LoggedIn = Template.bind({});
+export const LoadingBox = Template.bind({});
+
+LoadingBox.args = {
+  isLoading: true,
+  style: {
+    width: "100px",
+    height: "100px",
+    border: "1px solid #000",
+  },
+};
+
+export const NoDataBox = Template.bind({});
+
+NoDataBox.args = {
+  isLoading: false,
+  style: {
+    width: "200px",
+    height: "200px",
+    border: "1px solid #000",
+  },
+};
+export const hasDataBox = Template.bind({});
+
+hasDataBox.args = {
+  isLoading: false,
+  data: [12, 23],
+  children: <div>111</div>,
+  style: {
+    width: "200px",
+    height: "200px",
+    border: "1px solid #000",
+  },
+};
